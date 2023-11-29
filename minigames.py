074139541397d38d3224fd840007166d342e8b64
Lukeo <vippugster@gmail.com>
2023-11-29 14:16:10 +0000

    seperating minigames into seperate files

---
+++ minigames.py
@@ -0,0 +1,17 @@
+from random import randint
+
+#constants
+WOOD_STARTRANGE=0
+WOOD_ENDRANGE=50
+
+def wood_minigame():
+    for x in range(1):
+        wood_minigame_number1 = randint(WOOD_STARTRANGE, WOOD_ENDRANGE)
+        wood_minigame_number2 = randint(WOOD_STARTRANGE, WOOD_ENDRANGE)
+        wood_minigame_answer = int(input("what is " + str(wood_minigame_number1) + " + " + str(wood_minigame_number2) + " "))
+        if wood_minigame_answer == wood_minigame_number1 + wood_minigame_number2:
+            global wood_resource
+            wood_resource = wood_resource + 1
+            print("you succesfully cut down a tree you now have " + str(wood_resource) + " wood")
+        else:
+            print("wrong answer")
